520de028261c1e4feadf4e363c2dc45ef54fa405
Chris Mazzullo <chris.mazzullo@gmail.com>
2019-12-14 00:09:33 -0500

    Initial commit - threw together automata thing

---
+++ world.py
@@ -0,0 +1,91 @@
+"""
+One dimensional automata, a la Wolfram. Just for fun.
+"""
+
+import random
+
+
+def step(world, rule):
+    new_world = []
+    for i in range(len(world)):
+        new_world.append(rule(world[i], neighbors(world, i)))
+    return new_world
+
+
+def neighbors(world, index):
+    "Edges are set to 0 for now"
+    left = world[index - 1] if index > 0 else 0
+    right = world[index + 1] if index < len(world) - 1 else 0
+    return left + right
+
+
+def stringify(world):
+    return ''.join('X' if x else '.' for x in world)
+
+
+def random_world(n):
+    return random.choices([0, 1], k=n)
+
+
+def make_rule(rule_string):
+    def cell_rule(cell, neighbors):
+        if cell == 0:
+            if neighbors == 0:
+                return int(rule_string[0])
+            if neighbors == 1:
+                return int(rule_string[1])
+            if neighbors == 2:
+                return int(rule_string[2])
+        elif cell == 1:
+            if neighbors == 0:
+                return int(rule_string[3])
+            if neighbors == 1:
+                return int(rule_string[4])
+            if neighbors == 2:
+                return int(rule_string[5])
+    return cell_rule
+
+
+# Tests
+
+def assertListEqual(l1, l2):
+    for e1, e2 in zip(l1, l2):
+        assert e1 == e2
+
+
+assert neighbors([1, 0, 1], 2) == neighbors([1, 0, 1], 0) == 0
+assert neighbors([1, 0, 1], 1) == 2
+
+rule_string = '001110'
+new_rule = make_rule(rule_string)
+assert new_rule(0, 0) == 0
+assert new_rule(0, 1) == 0
+assert new_rule(0, 2) == 1
+assert new_rule(1, 0) == 1
+assert new_rule(1, 1) == 1
+assert new_rule(1, 2) == 0
+
+rule_string = '111000'
+new_rule = make_rule(rule_string)
+assert new_rule(0, 0) == 1
+assert new_rule(0, 1) == 1
+assert new_rule(0, 2) == 1
+assert new_rule(1, 0) == 0
+assert new_rule(1, 1) == 0
+assert new_rule(1, 2) == 0
+
+assertListEqual(step([1, 0, 1], make_rule('001110')), [1, 1, 1])
+
+assert stringify([0, 1, 1]) == '.XX'
+
+
+if __name__ == '__main__':
+    "Run a whole little simulation with all the rules"
+    generations = 100
+    world_size = 100
+    all_rules = [make_rule('{:06b}'.format(i)) for i in range(2**6)]
+    for rule in all_rules:
+        world = random_world(world_size)
+        for g in range(generations):
+            print(stringify(world))
+            world = step(world, rule)
